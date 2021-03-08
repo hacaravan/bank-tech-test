ef17ff503cfa72534d622b2ed2cce618ae6bdcb6
hacaravan <hacavanagh@gmail.com>
2021-03-08 15:04:57 +0000

    returning date from interaction

--- spec/interactionSpec.js
+++ spec/interactionSpec.js
@@ -6,5 +6,17 @@ describe('Interaction', () => {
       let deposit = new Interaction(100)
       expect(deposit.credit).toEqual(100)
     })
+    it("has today's date in a human format", () => {
+      let today = new Date
+      let day = today.getDate()
+      let month = today.getMonth() + 1
+      let year = today.getFullYear()
+      let dd = (day < 10 ? "0" : "") + day
+      let mm = (month < 10 ? "0" : "") + month
+      let yyyy = String(year)
+      let todayString = `${dd}/${mm}/${yyyy}`
+      let deposit = new Interaction(100)
+      expect(deposit.date).toEqual(todayString)
+    })
   })
 })
--- src/interaction.js
+++ src/interaction.js
@@ -3,5 +3,14 @@
 class Interaction {
   constructor(amount) {
     this.credit = amount;
+    let today = new Date
+    let day = today.getDate()
+    let month = today.getMonth() + 1
+    let year = today.getFullYear()
+    let dd = (day < 10 ? "0" : "") + day
+    let mm = (month < 10 ? "0" : "") + month
+    let yyyy = String(year)
+    let todayString = `${dd}/${mm}/${yyyy}`
+    this.date = todayString
   }
 }
