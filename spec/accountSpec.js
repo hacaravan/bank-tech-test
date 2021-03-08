12c31f704bfc43f4d9f5dc2c651d969761e728b9
hacaravan <hacavanagh@gmail.com>
2021-03-08 15:46:29 +0000

    Writing first test for deposit in accountSpec

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,12 +1,23 @@
 "use strict"
 
 describe ('Account', () => {
+  let account;
+  beforeEach( () => {
+    account = new Account
+  })
   describe('printStatement', () => {
     describe("when you haven't added any money yet", () => {
       it('prints just the headers', () => {
-        let account = new Account;
         expect(account.printStatement()).toEqual('date || credit || debit || balance')
       })
+    })
+  })
+
+  describe('deposit', () => {
+    it('adds a line to the statement with a credit and the date and increases the balance', () => {
+      account.deposit(100);
+      let todayString = dateToString(new Date)
+      expect(account.printStatement()).toEqual(`date || credit || debit || balance\n${todayString} || 100.00 || || 100.00`)
     })
   })
 })
