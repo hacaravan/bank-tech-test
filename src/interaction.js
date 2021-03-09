87f8c78d688e5dcd384a9bd11986f265cb6f4b5a
hacaravan <hacavanagh@gmail.com>
2021-03-09 10:58:40 +0000

    Extracting date handling out of interaction

--- src/interaction.js
+++ src/interaction.js
@@ -14,13 +14,6 @@ class Interaction {
   }
 
   todayString() {
-    let today = new Date
-    let day = today.getDate()
-    let month = today.getMonth() + 1
-    let year = today.getFullYear()
-    let dd = (day < 10 ? "0" : "") + day
-    let mm = (month < 10 ? "0" : "") + month
-    let yyyy = String(year)
-    return `${dd}/${mm}/${yyyy}`
+    return dateToString(new Date)
   }
 }
