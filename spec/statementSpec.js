41a719fa6ac1c1f1d33f517e7df949b2878b823a
hacaravan <hacavanagh@gmail.com>
2021-03-12 18:01:50 +0000

    Testing for multi line deposit/withdrawal

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -2,12 +2,14 @@
 
 describe("Statement", () => {
   describe("print()", () => {
-    let deposit, withdrawal;
+    let deposit, withdrawal, earlyDeposit;
     beforeEach(() => {
       deposit = jasmine.createSpyObj('deposit', ['transactionInfo'], {credit: 100})
       deposit.transactionInfo.and.returnValue('19/05/2021 || 100.00 || ||')
       withdrawal = jasmine.createSpyObj('withdrawal', ['transactionInfo'], {debit: 100})
       withdrawal.transactionInfo.and.returnValue('19/05/2021 || || 100.00 ||')
+      earlyDeposit = jasmine.createSpyObj('deposit', ['transactionInfo'], {credit: 100})
+      earlyDeposit.transactionInfo.and.returnValue('19/05/2020 || 100.00 || ||')
     })
     describe("for an empty array", () => {
       it('prints just the headers', () => {
@@ -26,8 +28,6 @@ describe("Statement", () => {
     })
     describe("for multiple values with credits", () => {
       it('prints the lines in the reverse of the order they were passed with the correct balance', () => {
-        let earlyDeposit = jasmine.createSpyObj('deposit', ['transactionInfo'], {credit: 100})
-        earlyDeposit.transactionInfo.and.returnValue('19/05/2020 || 100.00 || ||')
         expect(Statement.prototype.print([earlyDeposit, deposit])).toEqual('date || credit || debit || balance\n19/05/2021 || 100.00 || || 200.00\n19/05/2020 || 100.00 || || 100.00')
       })
     })
@@ -36,6 +36,11 @@ describe("Statement", () => {
         let earlyWithdrawal = jasmine.createSpyObj('withdrawal', ['transactionInfo'], {debit: 100})
         earlyWithdrawal.transactionInfo.and.returnValue('19/05/2020 || || 100.00 ||')
         expect(Statement.prototype.print([earlyWithdrawal, withdrawal])).toEqual('date || credit || debit || balance\n19/05/2021 || || 100.00 || -200.00\n19/05/2020 || || 100.00 || -100.00')
+      })
+    })
+    describe("for multiple values with credits and debits", () => {
+      it('prints the lines in the reverse of the order they were passed with the correct balance', () => {
+        expect(Statement.prototype.print([earlyDeposit, withdrawal, deposit])).toEqual('date || credit || debit || balance\n19/05/2021 || 100.00 || || 100.00\n19/05/2021 || || 100.00 || 0.00\n19/05/2020 || 100.00 || || 100.00')
       })
     })
     // describe('when you make multiple deposits', () => {
