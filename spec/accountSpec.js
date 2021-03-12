4f5d576822d748d84c4d233181289efd52362de6
hacaravan <hacavanagh@gmail.com>
2021-03-12 18:11:58 +0000

    Adding printStatement tests back into accountSpec

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -3,30 +3,55 @@
 "use strict"
 
 describe ('Account', () => {
-  let account, testDate;
+  let account, testDate, earlyDate;
   beforeEach( () => {
     testDate = new Date('2021-05-19')
+    earlyDate = new Date('2020-05-19')
     account = new Account
     jasmine.clock().install();
     jasmine.clock().mockDate(testDate)
   })
 
-  // it("deals with multiple", () => {
-  //   account.deposit(100, earlyDate);
-  //   account.deposit(100, earlyDate);
-  //   account.deposit(100, earlyDate);
-  //   account.deposit(100, earlyDate);
-  //   account.deposit(100, earlyDate);
-  //   account.deposit(200, testDate);
-  //   account.deposit(200, testDate);
-  //   account.deposit(200, testDate);
-  //   account.deposit(200, testDate);
-  //   account.deposit(200, testDate);
-  //   expect(account.printStatement()).toEqual(1)
-  // })
-
   describe('printStatement', () => {
-
+    describe('when you make multiple deposits', () => {
+      beforeEach( () => {
+        account.deposit(100, earlyDate);
+        account.deposit(200, testDate);
+      })
+      it('prints newer transactions first', () => {
+        expect(account.printStatement().indexOf("19/05/2021")).toBeLessThan(account.printStatement().indexOf("19/05/2020"))
+      })
+      it('increases the balance by the correct amount', () => {
+        let expectation = 'date || credit || debit || balance\n19/05/2021 || 200.00 || || 300.00\n19/05/2020 || 100.00 || || 100.00'
+        expect(account.printStatement()).toEqual(expectation)
+      })
+    })
+    describe('when you make multiple withdrawals', () => {
+      beforeEach( () => {
+        account.withdraw(100, earlyDate);
+        account.withdraw(200, testDate);
+      })
+      it('prints newer transactions first', () => {
+        expect(account.printStatement().indexOf("19/05/2021")).toBeLessThan(account.printStatement().indexOf("19/05/2020"))
+      })
+      it('decreases the balance by the correct amount', () => {
+        let expectation = 'date || credit || debit || balance\n19/05/2021 || || 200.00 || -300.00\n19/05/2020 || || 100.00 || -100.00'
+        expect(account.printStatement()).toEqual(expectation)
+      })
+    })
+    describe('when you deposit and withdraw', () => {
+      beforeEach( () => {
+        account.deposit(200, earlyDate);
+        account.withdraw(100, testDate);
+      })
+      it('prints newer transactions first', () => {
+        expect(account.printStatement().indexOf("19/05/2021")).toBeLessThan(account.printStatement().indexOf("19/05/2020"))
+      })
+      it('increases and decreases the balance by the correct amount', () => {
+        let expectation = 'date || credit || debit || balance\n19/05/2021 || || 100.00 || 100.00\n19/05/2020 || 200.00 || || 200.00'
+        expect(account.printStatement()).toEqual(expectation)
+      })
+    })
   })
 
   describe('deposit()', () => {
--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -43,44 +43,5 @@ describe("Statement", () => {
         expect(Statement.prototype.print([earlyDeposit, withdrawal, deposit])).toEqual('date || credit || debit || balance\n19/05/2021 || 100.00 || || 100.00\n19/05/2021 || || 100.00 || 0.00\n19/05/2020 || 100.00 || || 100.00')
       })
     })
-    // describe('when you make multiple deposits', () => {
-    //   beforeEach( () => {
-    //     account.deposit(100, earlyDate);
-    //     account.deposit(200, testDate);
-    //   })
-    //   it('prints newer transactions first', () => {
-    //     expect(account.printStatement().indexOf("19/05/2021")).toBeLessThan(account.printStatement().indexOf("19/05/2020"))
-    //   })
-    //   it('increases the balance by the correct amount', () => {
-    //     let expectation = 'date || credit || debit || balance\n19/05/2021 || 200.00 || || 300.00\n19/05/2020 || 100.00 || || 100.00'
-    //     expect(account.printStatement()).toEqual(expectation)
-    //   })
-    // })
-    // describe('when you make multiple withdrawals', () => {
-    //   beforeEach( () => {
-    //     account.withdraw(100, earlyDate);
-    //     account.withdraw(200, testDate);
-    //   })
-    //   it('prints newer transactions first', () => {
-    //     expect(account.printStatement().indexOf("19/05/2021")).toBeLessThan(account.printStatement().indexOf("19/05/2020"))
-    //   })
-    //   it('decreases the balance by the correct amount', () => {
-    //     let expectation = 'date || credit || debit || balance\n19/05/2021 || || 200.00 || -300.00\n19/05/2020 || || 100.00 || -100.00'
-    //     expect(account.printStatement()).toEqual(expectation)
-    //   })
-    // })
-    // describe('when you deposit and withdraw', () => {
-    //   beforeEach( () => {
-    //     account.deposit(200, earlyDate);
-    //     account.withdraw(100, testDate);
-    //   })
-    //   it('prints newer transactions first', () => {
-    //     expect(account.printStatement().indexOf("19/05/2021")).toBeLessThan(account.printStatement().indexOf("19/05/2020"))
-    //   })
-    //   it('increases and decreases the balance by the correct amount', () => {
-    //     let expectation = 'date || credit || debit || balance\n19/05/2021 || || 100.00 || 100.00\n19/05/2020 || 200.00 || || 200.00'
-    //     expect(account.printStatement()).toEqual(expectation)
-    //   })
-    // })
   })
 })
