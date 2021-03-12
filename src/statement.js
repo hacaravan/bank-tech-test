0f96c432c67293b1ad8c39360098a534dfd4ce07
hacaravan <hacavanagh@gmail.com>
2021-03-12 16:55:36 +0000

    Extracting addTransactionLines function

--- src/statement.js
+++ src/statement.js
@@ -2,20 +2,24 @@
 
 class Statement {
   print(transactions) {
-    let statement = 'date || credit || debit || balance'
-    transactions.slice().reverse().forEach((transaction, index, transactionsArray) => {
-      let balance = this.calculateBalance(transactionsArray, index)
-      statement += this.format(transaction, balance)
-    });
-    return statement
+    let statementLines = ['date || credit || debit || balance']
+    this.addTransactionLines(statementLines, transactions)
+    return statementLines.join('\n')
   }
 
   format(transaction, balance) {
-    return `\n${transaction.transacInfo()} ${balance.toFixed(2)}`
+    return `${transaction.transacInfo()} ${balance.toFixed(2)}`
   }
 
   calculateBalance(transactionsArray, index) {
     const reducer = (accumulator, transaction) => accumulator + (transaction.credit || 0) - (transaction.debit || 0)
     return transactionsArray.slice().reverse().slice(0, transactionsArray.length - index).reduce(reducer, 0)
+  }
+
+  addTransactionLines(statementLines, transactions){
+    transactions.slice().reverse().forEach((transaction, index, transactionsArray) => {
+      let balance = this.calculateBalance(transactionsArray, index)
+      statementLines.push(this.format(transaction, balance))
+    });
   }
 }
